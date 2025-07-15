b46366da4b6ba56b4a54b80e95cabaa3c996a18c
Tiozzo Mattia (Consulente) <M.Tiozzo.esterno@almaviva.it>
2025-07-15 17:42:42 +0200

    commit

--- e-commerce/src/app/pages/login.component/login.component.ts
+++ e-commerce/src/app/pages/login.component/login.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-login.component',
-  imports: [],
+  selector: 'login-component',
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  public username: string;
+  public password: string;
+
   login() {
-    console.log()
+    const user = {
+      username: this.username,
+      password: this.password
+    }
+    console.log(JSON.stringify(user));
   }
 }
